198eda215cfdccee679e161f2ae7d713ea631e01
Ali Shirani <alishiranii1384@gmail.com>
2021-09-21 12:48:02 -0700

    counter
    this the first js project

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let number= 0
+let count= document.getElementById("num")
+function plus(){
+    number+=1
+    count.innerText=number
+}
+function minees(){
+    number-=1
+    count.innerText=number
+}
