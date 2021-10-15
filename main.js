746a72f198122f9179997a62fc3fa20b18bdf254
Ali Shirani <87601568+alishirani1384@users.noreply.github.com>
2021-10-15 20:49:38 +0330

    updated
    this is the update version

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-let number= 0
-let count= document.getElementById("num")
-function plus(){
-    number+=1
-    count.innerText=number
-}
-function minees(){
-    number-=1
-    count.innerText=number
-}
+let number=0
+const value=document.getElementById("value")
+const btns=document.querySelectorAll(".btn")
+btns.forEach(function(btn){
+    btn.addEventListener('click',(e)=>{
+        const styles=e.currentTarget.classList
+        if (styles.contains('increase')) {
+            number++
+        }
+        else if (styles.contains('reset')) {
+            number=0 
+        }
+        if (styles.contains('decrease')) {
+            number--
+        }
+        if (number>0) {
+            value.style.color='green'  
+        }
+        if (number===0) {
+            value.style.color='black'  
+        }
+        if (number<0) {
+            value.style.color='red'  
+        }
+        value.textContent=number
+    })
+})
